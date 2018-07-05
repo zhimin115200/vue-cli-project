5bd00d57792c524a92af7767318bff59bbeebec1
zhimin115200 <786667916>
2018-07-05 11:20:20 +0800

    添加一些基础参照功能

---
+++ template/src/page/Navgitor.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a v-on:click="goPage1">
+          打开page1
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    goPage1: function () {
+      this.$router.push({path: `page1`,
+        query: {
+          msg: '测试传参'
+        }})
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
