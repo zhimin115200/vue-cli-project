ec3372c803c0db811d9d29185088a8fe31617900
zhimin115200 <786667916>
2018-07-05 15:02:41 +0800

    添加一些基础参照功能

---
+++ src/components/MyButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>子组件{{value}}</h1>
+    <button style="color: red;width: 100px;height: 100px" v-on:click="click">子组件点我</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MyButton',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    value () {
+      return this.$store.state.value
+    }
+  },
+  methods: {
+    click: function () {
+      this.$store.commit('inc')
+    }
+  }
+
+}
+</script>
--- src/page/page1.vue
+++ src/page/page1.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="hello">
     <a v-on:click="goback">返回</a>
-    <input v-focus/>
-    <h1>{{ msg }}</h1>
-    <h3>{{value | plus5}}</h3>
+    directive:<input v-focus/>
+    <h1>传参：{{ msg }}</h1>
+    <h3>filter：{{value | plus5}}</h3>
     <button v-on:click="callService">调用service</button>
+    <h3>父组件{{myButtonData}}</h3>
+    <MyButton></MyButton>
   </div>
 </template>
 
 <script>
 import PageService from '../service/page1.service.js'
+import MyButton from '../components/MyButton.vue'
 export default {
+  components: {
+    MyButton
+  },
   name: 'page1',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
       value: 12
+    }
+  },
+  computed: {
+    myButtonData () {
+      return this.$store.state.value
     }
   },
   created: function () {
