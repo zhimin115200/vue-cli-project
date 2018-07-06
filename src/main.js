76bb7cd2c43d14f7c13bd2d359bd30dc937a49d9
zhimin115200 <786667916>
2018-07-06 13:46:44 +0800

    添加restApi 实例

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import RestApi from 'xk-restapi'
 
 import directives from './common/directive/directive.js'
 import filters from './common/filter/filter.js'
@@ -13,6 +14,15 @@ Vue.use(directives)
 for (var key in filters) {
   Vue.filter(key, filters[key])
 }
+
+// 初始化restApi
+RestApi.init({
+  mode: 'dev', // 测试模式
+  baseUrl: 'http://t2.xikang.com/apphybrid/api/v3'
+})
+// RestApi.init({
+//   mode: 'pro' // 生产模式
+// })
 
 Vue.config.productionTip = false
 
