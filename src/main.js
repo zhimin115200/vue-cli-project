dec3543b94eec40d668458a340cbf0875539770e
zhimin115200 <786667916>
2018-07-13 11:23:27 +0800

    xk-restapi

--- src/main.js
+++ src/main.js
@@ -3,26 +3,31 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import RestApi from 'xk-restapi'
 
 import directives from './common/directive/directive.js'
 import filters from './common/filter/filter.js'
 import store from './store/store.js'
-
+import {interceptors} from 'xk-restapi'
 Vue.use(directives)
 
 for (var key in filters) {
   Vue.filter(key, filters[key])
 }
 
-// 初始化restApi
-RestApi.init({
-  mode: 'dev', // 测试模式
-  baseUrl: 'http://t2.xikang.com/apphybrid/api/v3'
-})
-// RestApi.init({
-//   mode: 'pro' // 生产模式
-// })
+interceptors.request.options = function (options) {
+  console.log(options)
+  return options
+}
+
+interceptors.response.success = function (response) {
+  console.log(response)
+  return response
+}
+
+interceptors.response.error = function (error) {
+  console.error(error)
+  return error
+}
 
 Vue.config.productionTip = false
 
